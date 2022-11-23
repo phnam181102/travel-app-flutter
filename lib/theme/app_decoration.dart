8fe749b05063d9b3a4a2e50a240e84fca8d214d3
phnam181102 <nam54882@donga.edu.vn>
2022-11-23 04:14:38 +0000

    Code Synced by DhiWise

---
+++ lib/theme/app_decoration.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:test/core/app_export.dart';
+
+class AppDecoration {
+  static BoxDecoration get outlineBluegray2001e1 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        boxShadow: [
+          BoxShadow(
+            color: ColorConstant.bluegray2001e1,
+            spreadRadius: getHorizontalSize(
+              2.00,
+            ),
+            blurRadius: getHorizontalSize(
+              2.00,
+            ),
+            offset: Offset(
+              0,
+              6,
+            ),
+          ),
+        ],
+      );
+  static BoxDecoration get outlineBluegray2001e => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        boxShadow: [
+          BoxShadow(
+            color: ColorConstant.bluegray2001e,
+            spreadRadius: getHorizontalSize(
+              2.00,
+            ),
+            blurRadius: getHorizontalSize(
+              2.00,
+            ),
+            offset: Offset(
+              0,
+              6,
+            ),
+          ),
+        ],
+      );
+  static BoxDecoration get fillWhiteA70090 => BoxDecoration(
+        color: ColorConstant.whiteA70090,
+      );
+  static BoxDecoration get fillWhiteA700 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+      );
+  static BoxDecoration get fillBlueA400 => BoxDecoration(
+        color: ColorConstant.blueA400,
+      );
+  static BoxDecoration get outlineBluegray20023 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        boxShadow: [
+          BoxShadow(
+            color: ColorConstant.bluegray20023,
+            spreadRadius: getHorizontalSize(
+              2.00,
+            ),
+            blurRadius: getHorizontalSize(
+              2.00,
+            ),
+            offset: Offset(
+              0,
+              6,
+            ),
+          ),
+        ],
+      );
+  static BoxDecoration get fillBlue50 => BoxDecoration(
+        color: ColorConstant.blue50,
+      );
+  static BoxDecoration get fillGray9006c => BoxDecoration(
+        color: ColorConstant.gray9006c,
+      );
+  static BoxDecoration get txtFillBlue50 => BoxDecoration(
+        color: ColorConstant.blue50,
+      );
+  static BoxDecoration get fillGray100 => BoxDecoration(
+        color: ColorConstant.gray100,
+      );
+  static BoxDecoration get outlineBluegray20028 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        boxShadow: [
+          BoxShadow(
+            color: ColorConstant.bluegray20028,
+            spreadRadius: getHorizontalSize(
+              2.00,
+            ),
+            blurRadius: getHorizontalSize(
+              2.00,
+            ),
+            offset: Offset(
+              0,
+              6,
+            ),
+          ),
+        ],
+      );
+  static BoxDecoration get fillWhiteA7006c => BoxDecoration(
+        color: ColorConstant.whiteA7006c,
+      );
+  static BoxDecoration get fillWhiteA7007e => BoxDecoration(
+        color: ColorConstant.whiteA7007e,
+      );
+}
+
+class BorderRadiusStyle {
+  static BorderRadius circleBorder22 = BorderRadius.circular(
+    getHorizontalSize(
+      22.00,
+    ),
+  );
+
+  static BorderRadius circleBorder12 = BorderRadius.circular(
+    getHorizontalSize(
+      12.00,
+    ),
+  );
+
+  static BorderRadius roundedBorder30 = BorderRadius.circular(
+    getHorizontalSize(
+      30.00,
+    ),
+  );
+
+  static BorderRadius roundedBorder0 = BorderRadius.circular(
+    getHorizontalSize(
+      0.50,
+    ),
+  );
+
+  static BorderRadius txtCircleBorder12 = BorderRadius.circular(
+    getHorizontalSize(
+      12.00,
+    ),
+  );
+
+  static BorderRadius circleBorder17 = BorderRadius.circular(
+    getHorizontalSize(
+      17.00,
+    ),
+  );
+}
